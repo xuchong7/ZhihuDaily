926f9baa1a803a1886566caa961fe62ff4567802
xu_cc <xu_cc@outlook.com>
2015-10-25 01:13:34 +0800

    MainPage

--- ZhihuDaily/MainPage.xaml.cs
+++ ZhihuDaily/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -22,9 +23,54 @@ namespace ZhihuDaily
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        ObservableCollection<ProfileItem> p_items = null;
+        ObservableCollection<SectionItem> s_items = null;
         public MainPage()
         {
             this.InitializeComponent();
+
+            p_items = new ObservableCollection<ProfileItem>();
+            s_items = new ObservableCollection<SectionItem>();
+
+            this.list_profile.ItemsSource = p_items;
+            this.list_section.ItemsSource = s_items;
         }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            p_items.Clear();
+            p_items.Add(new ProfileItem { Icon = "People", Content = "登录" });
+            p_items.Add(new ProfileItem { Icon = "Home", Content = "首页" });
+            p_items.Add(new ProfileItem { Icon = "Favorite", Content = "收藏" });
+
+            s_items.Clear();
+            s_items.Add(new SectionItem { Id = "1", Section = "日常心理学" });
+            s_items.Add(new SectionItem { Id = "1", Section = "用户推荐日报" });
+            s_items.Add(new SectionItem { Id = "1", Section = "电影日报" });
+            s_items.Add(new SectionItem { Id = "1", Section = "不许无聊" });
+            s_items.Add(new SectionItem { Id = "1", Section = "设计日报" });
+            s_items.Add(new SectionItem { Id = "1", Section = "大公司日报" });
+            s_items.Add(new SectionItem { Id = "1", Section = "财经日报" });
+            s_items.Add(new SectionItem { Id = "1", Section = "互联网安全" });
+            s_items.Add(new SectionItem { Id = "1", Section = "开始游戏" });
+            s_items.Add(new SectionItem { Id = "1", Section = "音乐日报" });
+            s_items.Add(new SectionItem { Id = "1", Section = "动漫日报" });
+            s_items.Add(new SectionItem { Id = "1", Section = "体育日报" });
+
+        }
+    }
+
+
+    public class ProfileItem
+    {
+        public string Icon { get; set; }
+        public string Content { get; set; }
+    }
+
+
+    public class SectionItem
+    {
+        public string Section { get; set; }
+        public string Id { get; set; }
     }
 }
